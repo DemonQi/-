fe7dc1fbb9fa138502a5700714ff71702d02e878
apple <apple@appledeMacBook-Air-2.local>
2015-07-24 20:05:13 +0800

    追女仔项目

---
+++ AXIB/AXIB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AXIB
+//
+//  Created by apple on 15/7/24.
+//  Copyright © 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
